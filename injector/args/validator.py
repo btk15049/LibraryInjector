ec07cf019ef5f258ad9a0a795c0e37fbd41ed59e
btk <btk15049@live.jp>
2019-10-24 23:07:23 +0900

    sonarしてないテストを追加してみる

---
+++ injector/args/validator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import argparse
+
+
+class ArgumentCombinationError(Exception):
+    pass
+
+
+def validate_if_see_only(args: argparse.Namespace):
+    if args.action == 'see-dependency' and args.inplace:
+        raise ArgumentCombinationError(
+            "If a '--action' value is 'see-dependency', '--inplace' flag should not be set.")
+    return
+
+
+def validate_args(args: argparse.Namespace):
+    validate_if_see_only(args)
