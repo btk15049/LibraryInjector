03b9aea708333836437ef0bcf810671af91d6c3b
btk <btk15049@live.jp>
2019-10-27 03:05:30 +0900

    coverageに含まれていない件を修正

--- tests/algorithm/test_topological_sort.py
+++ tests/algorithm/test_topological_sort.py
@@ -3,7 +3,7 @@ from injector.algorithm.topological_sort import topological_sort
 import unittest
 
 
-class ArgumentParserTest(unittest.TestCase):
+class TopologicalSortTest(unittest.TestCase):
     def test_topological_sort_with_list(self):
         nodes = [0, 1, 2]
         edges = [[1, 2], [], [1]]
@@ -15,3 +15,7 @@ class ArgumentParserTest(unittest.TestCase):
         edges = {'a': ['d'], 'b': ['c', 'a'], 'c': ['a'], 'd': []}
         expected = ['d', 'a', 'c', 'b']
         self.assertListEqual(topological_sort(nodes, edges), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
