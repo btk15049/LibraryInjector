043727ec78c27b1dc859a6ef3ece997b87c0ec5f
btk <btk15049@live.jp>
2019-12-01 20:27:43 +0900

    細かい入力ロジックは実装

--- injector/cppfile/info.py
+++ injector/cppfile/info.py
@@ -2,37 +2,77 @@
 import pathlib
 import re
 import os
+import logging
 
 
 class Include:
     local_matcher = re.compile(r'[\s]*#[\s]*include[\s]*\"([^\"]+)\"')
     stl_matcher = re.compile(r'[\s]*#[\s]*include[\s]*\<([^\>]+)\>')
 
-    def __init__(self, source: [str], lib_dirs: [str]) -> None:
-        headers = list(map(lambda h: h.group(1), filter(
-            lambda h: h is not None, map(
-                Include.local_matcher.search, source))))
-        self.local = []
-        for header in headers:
-            for lib_dir in lib_dirs:
-                if pathlib.Path(lib_dir + os.sep + header).exists():
-                    self.local.append(lib_dir + os.sep + header)
-        self.stl = list(map(lambda h: h.group(1), filter(
-            lambda h: h is not None, map(
-                Include.stl_matcher.search, source))))
+    def __init__(self, source: [str], lib_dirs: [str]):
+        self.local: {int, str} = {}
+        self.stl: {int, str} = {}
+        for si in range(len(source)):
+            # find local header
+            match_result = Include.local_matcher.search(source[si])
+            if match_result is not None:
+                header = match_result.group(1)
+                for lib_dir in lib_dirs:
+                    path = pathlib.Path(lib_dir + os.sep + header)
+                    if path.exists():
+                        self.local[si] = path
+            # get stl
+            match_result = Include.stl_matcher.search(source[si])
+            if match_result is not None:
+                header = match_result.group(1)
+                self.stl[si] = header
+
+
+class Implement:
+    def __init__(self, source: [str], *args):
+        self.implement: {int, str} = {}
+        excludes = set()
+        for arg in args:
+            for i in arg:
+                excludes.add(i)
+        for si in range(len(source)):
+            if si not in excludes:
+                self.implement[si] = source[si]
 
 
 class Source:
     cache = {}
+    hpps = set()
+    cpps = set()
 
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str):
         self.path = pathlib.Path(path)
-        self.cpp = Source.load(self.path.stem + '.cpp')
-        self.hpp = Source.load(self.path.stem + '.hpp')
-        self.includes = Include(self.path.read_text().splitlines())
+
+        base = str(self.path.parent) + os.sep + self.path.stem
+
+        hpp_name = base + '.hpp'
+        self.hpp = Source.load(hpp_name)
+        if self.hpp is not None:
+            Source.hpps.add(hpp_name)
+
+        cpp_name = base + '.cpp'
+        self.cpp = Source.load(cpp_name)
+        if self.cpp is not None:
+            Source.cpps.add(cpp_name)
+
+        self.content: [str] = self.path.read_text().splitlines()
+        self.includes = Include(
+            self.content,
+            [])  # TODO: assign lib_dir
+
+        self.implement = Implement(
+            self.content, self.includes.stl.keys(), self.includes.local.keys())
+        logging.info('{} is registered'.format(path))
 
     @staticmethod
     def load(path: str):
+        if not pathlib.Path(path).exists():
+            return None
         if path not in Source.cache:
             Source.cache[path] = Source(path)
         return Source.cache[path]
--- tests/cppfile/test_info.py
+++ tests/cppfile/test_info.py
@@ -2,29 +2,24 @@
 from injector.cppfile.info import *
 import unittest
 import tempfile
-from typing import List, Dict
+import os
+from unittest.mock import patch, call, MagicMock
 
 
-class ExtractIncludeTest(unittest.TestCase):
+class InfoTest(unittest.TestCase):
     def setUp(self) -> None:
         self.root = tempfile.TemporaryDirectory()
 
     def tearDown(self) -> None:
         self.root.cleanup()
 
-    def sandbox_test(self, files: [str], args: [
-                     str], expected: Dict[str, List[str]]):
+    def setUpFiles(self, files: [str]) -> None:
         for f_name in files:
             p = pathlib.Path(self.root.name + os.sep + f_name)
             p.parent.mkdir(parents=True, exist_ok=True)
             p.touch(exist_ok=True)
-        actual = Include(
-            args['source'],
-            args['lib_dirs'])
-        self.assertEqual(expected['local'], actual.local)
-        self.assertEqual(expected['stl'], actual.stl)
 
-    def test_extract_local_file(self):
+    def test_include(self):
         args = {
             'source': [
                 '#include "a.hpp"',
@@ -51,16 +46,132 @@ class ExtractIncludeTest(unittest.TestCase):
         ]
         expected = {
             'local': [
-                self.root.name + os.sep + 'lib1/a.hpp',
-                self.root.name + os.sep + 'lib1/b.hpp',
-                self.root.name + os.sep + 'lib1/A/a.hpp',
-                self.root.name + os.sep + 'lib1/A/b.hpp',
-                self.root.name + os.sep + 'lib1/B/a.hpp',
-                self.root.name + os.sep + 'lib2/c.hpp',
-                self.root.name + os.sep + 'lib2/A/d.hpp',
+                (0, pathlib.Path(self.root.name + os.sep + 'lib1/a.hpp')),
+                (1, pathlib.Path(self.root.name + os.sep + 'lib1/b.hpp')),
+                (2, pathlib.Path(self.root.name + os.sep + 'lib1/A/a.hpp')),
+                (3, pathlib.Path(self.root.name + os.sep + 'lib1/A/b.hpp')),
+                (4, pathlib.Path(self.root.name + os.sep + 'lib1/B/a.hpp')),
+                (5, pathlib.Path(self.root.name + os.sep + 'lib2/c.hpp')),
+                (6, pathlib.Path(self.root.name + os.sep + 'lib2/A/d.hpp')),
             ],
             'stl': [
-                'iostream'
+                (7, 'iostream')
             ],
         }
-        self.sandbox_test(files, args, expected)
+        self.setUpFiles(files)
+        actual = Include(
+            source=args['source'],
+            lib_dirs=args['lib_dirs'])
+        self.assertEqual(expected['local'], actual.local)
+        self.assertEqual(expected['stl'], actual.stl)
+
+    def test_implement(self):
+        source = [
+            'a',
+            'bb',
+            'ccc',
+            '   dd',
+        ]
+        impl = Implement(source)
+        expected = {
+            0: 'a',
+            1: 'bb',
+            2: 'ccc',
+            3: '   dd',
+        }
+        self.assertEqual(expected, impl.implement)
+        expected = {
+            1: 'bb',
+            2: 'ccc',
+            3: '   dd',
+        }
+        impl = Implement(source, [0])
+        self.assertEqual(expected, impl.implement)
+        expected = {
+            1: 'bb',
+            3: '   dd',
+        }
+        impl = Implement(source, [0], [0, 2])
+        self.assertEqual(expected, impl.implement)
+
+    def test_source(self):
+        files = [
+            'a.cpp',
+        ]
+        self.setUpFiles(files)
+
+        with patch('injector.cppfile.info.Include', MagicMock(return_value=MagicMock(stl={}, local={}))) as include:
+            with patch('injector.cppfile.info.Source.load', MagicMock(return_value=None)) as load:
+                with patch('injector.cppfile.info.Implement', MagicMock(return_value=MagicMock())) as implement:
+                    cpp = self.root.name + os.sep + 'a.cpp'
+                    hpp = self.root.name + os.sep + 'a.hpp'
+                    actual = Source(cpp)
+
+                    # check include
+                    self.assertEqual(1, include.call_count)
+                    self.assertIsNone(actual.hpp)
+                    self.assertIsNone(actual.cpp)
+
+                    # check load
+                    calls = [
+                        call(hpp),
+                        call(cpp),
+                    ]
+                    expected_hpps_set = set()
+                    expected_cpps_set = set()
+                    self.assertEqual(2, load.call_count)
+                    self.assertEqual(expected_hpps_set, Source.hpps)
+                    self.assertEqual(expected_cpps_set, Source.cpps)
+                    load.assert_has_calls(calls, any_order=True)
+
+                    # check implement
+                    self.assertEqual(1, implement.call_count)
+
+        with patch('injector.cppfile.info.Include', MagicMock(return_value=MagicMock(stl={}, local={}))) as include:
+            with patch('injector.cppfile.info.Source.load', MagicMock(return_value='mock')) as load:
+                with patch('injector.cppfile.info.Implement', MagicMock(return_value=MagicMock())) as implement:
+                    cpp = self.root.name + os.sep + 'a.cpp'
+                    hpp = self.root.name + os.sep + 'a.hpp'
+                    actual = Source(cpp)
+
+                    # check include
+                    self.assertEqual(1, include.call_count)
+                    self.assertEqual('mock', actual.hpp)
+                    self.assertEqual('mock', actual.cpp)
+
+                    # check load
+                    calls = [
+                        call(hpp),
+                        call(cpp),
+                    ]
+                    self.assertEqual(2, load.call_count)
+                    expected_hpps_set = set()
+                    expected_cpps_set = set()
+                    expected_hpps_set.add(hpp)
+                    expected_cpps_set.add(cpp)
+                    self.assertEqual(expected_hpps_set, Source.hpps)
+                    self.assertEqual(expected_cpps_set, Source.cpps)
+                    load.assert_has_calls(calls, any_order=True)
+
+                    # check implement
+                    self.assertEqual(1, implement.call_count)
+
+    def test_source_load(self):
+        self.setUpFiles(['mock/cache', 'mock/mock'])
+        Source.cache[self.root.name + os.sep + 'mock/cache'] = 'cache'
+        Source.cache['none'] = None
+        # file is not found
+        with patch('injector.cppfile.info.Source.__init__', MagicMock(return_value=None)) as source:
+            actual = Source.load('none')
+            self.assertIsNone(actual)
+            self.assertEqual(0, source.call_count)
+        # use cache
+        with patch('injector.cppfile.info.Source.__init__', MagicMock(return_value=None)) as source:
+            actual = Source.load(self.root.name + os.sep + 'mock/cache')
+            self.assertEqual('cache', actual)
+            self.assertEqual(0, source.call_count)
+        # use constructor
+        with patch('injector.cppfile.info.Source.__init__', MagicMock(return_value=None)) as source:
+            actual = Source.load(self.root.name + os.sep + 'mock/mock')
+            self.assertIsNotNone(actual)
+            self.assertEqual(source.call_count, 1)
