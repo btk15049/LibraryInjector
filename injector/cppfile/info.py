8d0ea02dd62a00d0acf36c063e4cdc5a31cd85e6
btk <btk15049@live.jp>
2019-12-27 23:12:30 +0900

    パースを実装

--- injector/cppfile/info.py
+++ injector/cppfile/info.py
@@ -3,6 +3,7 @@ import pathlib
 import re
 import os
 import logging
+from injector.algorithm.topological_sort import topological_sort
 
 
 class Include:
@@ -18,9 +19,9 @@ class Include:
             if match_result is not None:
                 header = match_result.group(1)
                 for lib_dir in lib_dirs:
-                    path = pathlib.Path(lib_dir + os.sep + header)
+                    path = pathlib.Path(lib_dir) / header
                     if path.exists():
-                        self.local[si] = path
+                        self.local[si] = str(path)
             # get stl
             match_result = Include.stl_matcher.search(source[si])
             if match_result is not None:
@@ -51,20 +52,26 @@ class Source:
         base = str(self.path.parent) + os.sep + self.path.stem
 
         hpp_name = base + '.hpp'
-        self.hpp = Source.load(hpp_name)
+        if path == hpp_name:
+            self.hpp = self
+        else:
+            self.hpp = Source.load(hpp_name, *lib_dirs)
         if self.hpp is not None:
             Source.hpps.add(hpp_name)
 
         cpp_name = base + '.cpp'
-        self.cpp = Source.load(cpp_name)
+        if path == cpp_name:
+            self.cpp = self
+        else:
+            self.cpp = Source.load(cpp_name, *lib_dirs)
         if self.cpp is not None:
             Source.cpps.add(cpp_name)
 
         self.content: [str] = self.path.read_text().splitlines()
         self.includes = Include(
-            self.content,
-            [], lib_dirs)
-
+            self.content, *lib_dirs)
+        for v in self.includes.local.values():
+            self.load(v, *lib_dirs)
         self.implement = Implement(
             self.content, self.includes.stl.keys(), self.includes.local.keys())
         logging.info('{} is registered'.format(path))
@@ -74,10 +81,32 @@ class Source:
         if not pathlib.Path(path).exists():
             return None
         if path not in Source.cache:
+            Source.cache[path] = None
             Source.cache[path] = Source(path, *lib_dirs)
         return Source.cache[path]
 
+    @staticmethod
+    def clear_cache():
+        Source.hpps = set()
+        Source.cpps = set()
+
 
 class Info:
-    def __init__(self, path, *lib_dirs) -> None:
-        pass
+    def __init__(self, path: str, *lib_dirs) -> None:
+        self.root: str = path
+        self.sources: [str] = []
+        self.lib_dirs = lib_dirs
+        Source.clear_cache()
+        # Sourceクラスのキャッシュに関連ファイルを読ませておく
+        Source.load(path, *self.lib_dirs)
+        self.sources.extend(list(Source.hpps))
+        self.sources.extend(list(Source.cpps))
+
+    def compute_sorted(self) -> [Source]:
+        edges = {
+            source: Source.load(
+                source,
+                self.lib_dirs).includes.local.values() for source in self.sources}
+        logging.info('nodes: ' + str(self.sources))
+        logging.info('edges: ' + str(edges))
+        return topological_sort(self.sources, edges)
--- tests/cppfile/test_info.py
+++ tests/cppfile/test_info.py
@@ -11,13 +11,18 @@ class InfoTest(unittest.TestCase):
         self.root = tempfile.TemporaryDirectory()
 
     def tearDown(self) -> None:
+        Source.clear_cache()
         self.root.cleanup()
 
-    def setUpFiles(self, files: [str]) -> None:
-        for f_name in files:
-            p = pathlib.Path(self.root.name + os.sep + f_name)
+    def setUpFiles(self, files: [(str, str)]) -> None:
+        for (f_name, content) in files:
+            p = pathlib.Path(self.root.name) / f_name
             p.parent.mkdir(parents=True, exist_ok=True)
             p.touch(exist_ok=True)
+            p.write_bytes(content.encode())
+
+    def getAbsPath(self, path) -> str:
+        return str(pathlib.Path(self.root.name) / path)
 
     def test_include(self):
         args = {
@@ -36,23 +41,23 @@ class InfoTest(unittest.TestCase):
             ]
         }
         files = [
-            'lib1/a.hpp',
-            'lib1/b.hpp',
-            'lib1/A/a.hpp',
-            'lib1/A/b.hpp',
-            'lib1/B/a.hpp',
-            'lib2/c.hpp',
-            'lib2/A/d.hpp',
+            ('lib1/a.hpp', ''),
+            ('lib1/b.hpp', ''),
+            ('lib1/A/a.hpp', ''),
+            ('lib1/A/b.hpp', ''),
+            ('lib1/B/a.hpp', ''),
+            ('lib2/c.hpp', ''),
+            ('lib2/A/d.hpp', ''),
         ]
         expected = {
             'local': {
-                0: pathlib.Path(self.root.name + os.sep + 'lib1/a.hpp'),
-                1: pathlib.Path(self.root.name + os.sep + 'lib1/b.hpp'),
-                2: pathlib.Path(self.root.name + os.sep + 'lib1/A/a.hpp'),
-                3: pathlib.Path(self.root.name + os.sep + 'lib1/A/b.hpp'),
-                4: pathlib.Path(self.root.name + os.sep + 'lib1/B/a.hpp'),
-                5: pathlib.Path(self.root.name + os.sep + 'lib2/c.hpp'),
-                6: pathlib.Path(self.root.name + os.sep + 'lib2/A/d.hpp'),
+                0: self.getAbsPath('lib1/a.hpp'),
+                1: self.getAbsPath('lib1/b.hpp'),
+                2: self.getAbsPath('lib1/A/a.hpp'),
+                3: self.getAbsPath('lib1/A/b.hpp'),
+                4: self.getAbsPath('lib1/B/a.hpp'),
+                5: self.getAbsPath('lib2/c.hpp'),
+                6: self.getAbsPath('lib2/A/d.hpp'),
             },
             'stl': {
                 7: 'iostream',
@@ -96,30 +101,30 @@ class InfoTest(unittest.TestCase):
 
     def test_source(self):
         files = [
-            'a.cpp',
+            ('a.cpp', '')
         ]
         self.setUpFiles(files)
 
         with patch('injector.cppfile.info.Include', MagicMock(return_value=MagicMock(stl={}, local={}))) as include:
             with patch('injector.cppfile.info.Source.load', MagicMock(return_value=None)) as load:
                 with patch('injector.cppfile.info.Implement', MagicMock(return_value=MagicMock())) as implement:
-                    cpp = self.root.name + os.sep + 'a.cpp'
-                    hpp = self.root.name + os.sep + 'a.hpp'
+                    cpp = self.getAbsPath('a.cpp')
+                    hpp = self.getAbsPath('a.hpp')
                     actual = Source(cpp)
 
                     # check include
                     self.assertEqual(1, include.call_count)
                     self.assertIsNone(actual.hpp)
-                    self.assertIsNone(actual.cpp)
+                    self.assertEqual(actual.cpp, actual)
 
                     # check load
                     calls = [
                         call(hpp),
-                        call(cpp),
                     ]
                     expected_hpps_set = set()
                     expected_cpps_set = set()
-                    self.assertEqual(2, load.call_count)
+                    expected_cpps_set.add(cpp)
+                    self.assertEqual(1, load.call_count)
                     self.assertEqual(expected_hpps_set, Source.hpps)
                     self.assertEqual(expected_cpps_set, Source.cpps)
                     load.assert_has_calls(calls, any_order=True)
@@ -137,14 +142,13 @@ class InfoTest(unittest.TestCase):
                     # check include
                     self.assertEqual(1, include.call_count)
                     self.assertEqual('mock', actual.hpp)
-                    self.assertEqual('mock', actual.cpp)
+                    self.assertEqual(actual.cpp, actual)
 
                     # check load
                     calls = [
                         call(hpp),
-                        call(cpp),
                     ]
-                    self.assertEqual(2, load.call_count)
+                    self.assertEqual(1, load.call_count)
                     expected_hpps_set = set()
                     expected_cpps_set = set()
                     expected_hpps_set.add(hpp)
@@ -157,7 +161,7 @@ class InfoTest(unittest.TestCase):
                     self.assertEqual(1, implement.call_count)
 
     def test_source_load(self):
-        self.setUpFiles(['mock/cache', 'mock/mock'])
+        self.setUpFiles([('mock/cache', ''), ('mock/mock', '')])
         Source.cache[self.root.name + os.sep + 'mock/cache'] = 'cache'
         Source.cache['none'] = None
         # file is not found
@@ -175,3 +179,50 @@ class InfoTest(unittest.TestCase):
             actual = Source.load(self.root.name + os.sep + 'mock/mock')
             self.assertIsNotNone(actual)
             self.assertEqual(source.call_count, 1)
+
+    def test_info(self):
+        files = [
+            ('lib1/a.hpp', '#include "B/a.hpp"'),
+            ('lib1/a.cpp', '#include "a.hpp"\n#include "b.hpp"\n'),
+            ('lib1/b.hpp', '#include "A/a.hpp"\n'),
+            ('lib1/A/a.hpp', '#include "A/b.hpp"\n#include "B/a.hpp\n"'),
+            ('lib1/A/b.hpp', '#include <iostream>\n'),
+            ('lib1/B/a.hpp', '#include <sstream>'),
+            ('lib2/c.hpp', '#include <algorithm>'),
+            ('lib2/A/d.hpp', '#include <fstream>'),
+            ('lib2/A/d.cpp', '#include "c.hpp"'),
+            ('main.cpp', '#include "A/d.hpp"\n#include "a.hpp"\n'),
+        ]
+        self.setUpFiles(files)
+        info = Info(
+            self.getAbsPath('main.cpp'),
+            self.getAbsPath('lib1'),
+            self.getAbsPath('lib2'))
+        expected_info = [
+            self.getAbsPath('main.cpp'),
+            self.getAbsPath('lib1/a.hpp'),
+            self.getAbsPath('lib1/a.cpp'),
+            self.getAbsPath('lib1/b.hpp'),
+            self.getAbsPath('lib1/A/a.hpp'),
+            self.getAbsPath('lib1/A/b.hpp'),
+            self.getAbsPath('lib1/B/a.hpp'),
+            self.getAbsPath('lib2/c.hpp'),
+            self.getAbsPath('lib2/A/d.hpp'),
+            self.getAbsPath('lib2/A/d.cpp')
+        ]
+        self.assertSetEqual(set(info.sources), set(expected_info))
+
+        topology = [
+            (self.getAbsPath('lib1/B/a.hpp'), self.getAbsPath('lib1/a.hpp')),
+            (self.getAbsPath('lib1/a.hpp'), self.getAbsPath('lib1/a.cpp')),
+            (self.getAbsPath('lib1/b.hpp'), self.getAbsPath('lib1/a.cpp')),
+            (self.getAbsPath('lib1/A/a.hpp'), self.getAbsPath('lib1/b.hpp')),
+            (self.getAbsPath('lib1/A/b.hpp'), self.getAbsPath('lib1/A/a.hpp')),
+            (self.getAbsPath('lib1/B/a.hpp'), self.getAbsPath('lib1/A/a.hpp')),
+            (self.getAbsPath('lib2/c.hpp'), self.getAbsPath('lib2/A/d.cpp')),
+            (self.getAbsPath('lib2/A/d.hpp'), self.getAbsPath('main.cpp')),
+            (self.getAbsPath('lib1/a.hpp'), self.getAbsPath('main.cpp'))
+        ]
+        actual = info.compute_sorted()
+        for before, after in topology:
+            self.assertLess(actual.index(before), actual.index(after))
